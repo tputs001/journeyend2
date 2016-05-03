b9d4b3f22739cdf6c3a1bd579b0c3da62ad6ea6b
Tom <tputs001@gmail.com>
2016-05-02 18:13:56 -0700

    Issue #4, I created a webscraper to scrape the restaurant pricing for each data being retrieved from yelp.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser =require('body-parser').json()
 var mongo = require('mongodb')
 var request = require('request');
 var yelp = require('./public/yelp');
+var scraper = require('./public/scraper')
 var myClient = mongo.MongoClient;
 var url = 'mongodb://localhost/suggest'
 var app = express();
@@ -55,17 +56,19 @@ app.get('/hikes/:query/', function(req, res){
   var location = req.params.query
   var results = yelp.yelpSearch(term, location)
   results.then(function(data){
-    insert(data, location, "hiking", res)
+    var activityArray = insert(data, location, "hiking", res)
+    getData(location, res, term, activityArray)
   })
 })
 
 app.get('/restaurant/:query', function(req, res){
   var location = req.params.query
   var term = "restaurants"
-  var results = yelp.yelpSearch(term, location)
-  results.then(function(data){
-    insert(data, location, "restaurants", res)
-  })
+    var results = yelp.yelpSearch(term, location)
+    results.then(function(data){
+      var activityArray = insert(data, location, "restaurants", res)
+      getData(location, res, term, activityArray)
+    })
 })
 
 app.get('/nightlife/:query', function(req, res){
@@ -73,7 +76,8 @@ app.get('/nightlife/:query', function(req, res){
   var term = "nightlife"
   var results = yelp.yelpSearch(term, location)
   results.then(function(data){
-    insert(data, location, "nightlife", res)
+    var activityArray = insert(data, location, "nightlife", res)
+    getData(location, res, term, activityArray)
   })
 })
 
@@ -82,7 +86,8 @@ app.get('/museums/:query', function(req, res){
   var term = "museums"
   var results = yelp.yelpSearch(term, location)
   results.then(function(data){
-    insert(data, location, "museums", res)
+    var activityArray = insert(data, location, "museums", res)
+    getData(location, res, term, activityArray)
   })
 })
 
@@ -90,12 +95,17 @@ function insert(data, location, activity, res){
   var activityArray = []
   for(var i = 0; i<data.businesses.length; i++){
     var img_url = data.businesses[i].image_url
-    var newUrl = img_url.slice(0, img_url.indexOf('/ms')) + '/o.jpg'
+    var newUrl;
+    if(img_url == undefined){
+      newUrl = "Not Available"
+    } else {
+      newUrl = img_url.slice(0, img_url.indexOf('/ms')) + '/o.jpg'
+    }
     activityArray.push({
       title : data.businesses[i].name,
       id : data.businesses[i].id,
       url : newUrl,
-      price : 0,
+      price : scraper.scrape(data.businesses[i].url, location, data.businesses[i].name, activity),
       categories : data.businesses[i].categories,
       latlng : data.businesses[i].location.cordinate,
       location : data.businesses[i].location,
@@ -103,19 +113,28 @@ function insert(data, location, activity, res){
       phone : data.businesses[i].phone
     })
   }
+  return activityArray;
+}
 
+function getData(location, res, terms, activityArray){
   myClient.connect(url, function(err, db){
     var database = db.collection('activities')
-    database.update(
-      {'location' : location},
-      { $set:
-        {
-          activity : activityArray
-        }
-      }, function(error, results){
-        res.send(activityArray)
+    database.findOne({location: location}, function(error, results){
+      var setObject = {}
+      setObject[terms] = activityArray
+      if(results[terms] == undefined || results[terms] == null){
+        database.update(
+          {'location' : location},
+          { $set:
+            setObject
+          }, function(error, results){
+            res.send(activityArray)
+          }
+        )
+      } else {
+        res.send(results[terms])
       }
-    )
+    })
   })
 }
 
